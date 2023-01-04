c996c63751acc15bcb661ba79e061c44d9fd3c0d
Alyona8891 <besssta888@mail.ru>
2023-01-04 11:08:52 +0300

    fix: correct solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 module.exports = function reverse (n) {
-    let nModule =  Math.abs(n);
-    let nString = nModule.toString();
-    let result = '';
-    
-  for (i =(nString.length-1); i >= 0; i = i - 1) {
-    result = `${result}${nString[i]}`;
-  }
-  return result;
+let nModule =  Math.abs(n);
+let nString = nModule.toString();
+let result = '';
+for (i =(nString.length-1); i >= 0; i = i - 1) {
+result = `${result}${nString[i]}`;
+}
+return result;
 }

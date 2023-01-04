d7f6482ee5cb5caf3a71020130062db43adaab9f
Alyona8891 <besssta888@mail.ru>
2023-01-04 11:06:42 +0300

    feat: add solutions at index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let nModule =  Math.abs(n);
+    let nString = nModule.toString();
+    let result = '';
+    
+  for (i =(nString.length-1); i >= 0; i = i - 1) {
+    result = `${result}${nString[i]}`;
+  }
+  return result;
 }
